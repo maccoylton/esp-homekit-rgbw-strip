3ee1f79b4b0b49d5fc35ee0de7d5018c838ad424
maccoylton <dave@maccoylton.com>
2018-06-19 00:12:30 +0100

    update code to convert hsi2rgb and scale factor

--- src/main.c
+++ src/main.c
@@ -36,10 +36,12 @@
 #define LPF_SHIFT 4  // divide by 16
 #define LPF_INTERVAL 10  // in milliseconds
 
+
+#define WHITE_PWM_PIN 13
+#define BLUE_PWM_PIN 12
 #define RED_PWM_PIN 5
-#define GREEN_PWM_PIN 12
-#define BLUE_PWM_PIN 13
-#define LED_RGB_SCALE 255       // this is the scaling factor used for color conversion
+#define GREEN_PWM_PIN 14
+#define LED_RGB_SCALE 32768       // this is the scaling factor used for color conversion
 
 // add this section to make your device OTA capable
 // create the extra characteristic &ota_trigger, at the end of the primary service (before the NULL)
@@ -63,10 +65,10 @@ homekit_characteristic_t revision     = HOMEKIT_CHARACTERISTIC_(FIRMWARE_REVISIO
 
 typedef union {
     struct {
+        uint16_t white;
         uint16_t blue;
         uint16_t green;
         uint16_t red;
-        uint16_t white;
     };
     uint64_t color;
 } rgb_color_t;
@@ -80,9 +82,30 @@ float led_hue = 0;              // hue is scaled 0 to 360
 float led_saturation = 59;      // saturation is scaled 0 to 100
 float led_brightness = 100;     // brightness is scaled 0 to 100
 bool led_on = false;            // on is boolean on or off
+uint8_t pins[] = {WHITE_PWM_PIN, BLUE_PWM_PIN,GREEN_PWM_PIN, RED_PWM_PIN};
+pwm_info_t pwm_info;
 
+
+
+double __ieee754_remainder(double x, double y) {
+    return x - y * floor(x/y);
+}
+
+
+void led_strip_init(){
+
+    multipwm_init(&pwm_info);
+    multipwm_set_freq(&pwm_info, 65535);
+    for (uint8_t i=0; i<pwm_info.channels; i++) {
+        multipwm_set_pin(&pwm_info, i, pins[i]);
+        printf ("Set pin %d \n",i);
+    }
+}
+    
+
+#define DEG_TO_RAD(X) (M_PI*(X)/180)
 //http://blog.saikoled.com/post/44677718712/how-to-convert-from-hsi-to-rgb-white
-static void hsi2rgb(float h, float s, float i, rgb_color_t* rgb) {
+void hsi2rgbw(float h, float s, float i, rgb_color_t* rgbw) {
     int r, g, b;
     
     while (h < 0) { h += 360.0F; };     // cycle h around to 0-360 degrees
@@ -112,32 +135,109 @@ static void hsi2rgb(float h, float s, float i, rgb_color_t* rgb) {
         g = LED_RGB_SCALE * i / 3 * (1 - s);
     }
     
-    rgb->red = (uint8_t) r;
-    rgb->green = (uint8_t) g;
-    rgb->blue = (uint8_t) b;
+    rgbw->red = (uint8_t) r;
+    rgbw->green = (uint8_t) g;
+    rgbw->blue = (uint8_t) b;
+    rgbw->white=0;
+}
+
+void hsi2rgbw_hack(float h, float s, float i, rgb_color_t* rgbw) {
+    uint16_t r, g, b, w;
+    float cos_h, cos_1047_h;
+    //h = fmod(h,360); // cycle h around to 0-360 degrees
+    h = 3.14159*h/(float)180; // Convert to radians.
+    s /=(float)100; i/=(float)100; //from percentage to ratio
+    s = s>0?(s<1?s:1):0; // clamp s and i to interval [0,1]
+    i = i>0?(i<1?i:1):0;
+    i = i*sqrt(i); //shape intensity to have finer granularity near 0
+    
+    if(h < 2.09439) {
+        cos_h = cos(h);
+        cos_1047_h = cos(1.047196667-h);
+        r = s*LED_RGB_SCALE*i/3*(1+cos_h/cos_1047_h);
+        g = s*LED_RGB_SCALE*i/3*(1+(1-cos_h/cos_1047_h));
+        b = 0;
+        w = LED_RGB_SCALE*(1-s)*i;
+    } else if(h < 4.188787) {
+        h = h - 2.09439;
+        cos_h = cos(h);
+        cos_1047_h = cos(1.047196667-h);
+        g = s*LED_RGB_SCALE*i/3*(1+cos_h/cos_1047_h);
+        b = s*LED_RGB_SCALE*i/3*(1+(1-cos_h/cos_1047_h));
+        r = 0;
+        w = LED_RGB_SCALE*(1-s)*i;
+    } else {
+        h = h - 4.188787;
+        cos_h = cos(h);
+        cos_1047_h = cos(1.047196667-h);
+        b = s*LED_RGB_SCALE*i/3*(1+cos_h/cos_1047_h);
+        r = s*LED_RGB_SCALE*i/3*(1+(1-cos_h/cos_1047_h));
+        g = 0;
+        w = LED_RGB_SCALE*(1-s)*i;
+    }
+    
+    rgbw->red=r;
+    rgbw->green=g;
+    rgbw->blue=b;
+    rgbw->white=w;
+}
+
+void led_strip_send (rgb_color_t *color){
+    
+    multipwm_stop(&pwm_info);
+    multipwm_set_duty(&pwm_info, 0, color->white);
+    multipwm_set_duty(&pwm_info, 1, color->blue);
+    multipwm_set_duty(&pwm_info, 2, color->green);
+    multipwm_set_duty(&pwm_info, 3, color->red);
+    printf ("sending r=%d,g=%d, b=%d, w=%d,\n", color->red,color->green, color->blue, color->white );
+    multipwm_start(&pwm_info);
+    
+}
+
+void led_strip_set() {
+
+    if (led_on) {
+        // convert HSI to RGBW
+        hsi2rgbw_hack(led_hue, led_saturation, led_brightness, &target_color);
+        printf("h=%d,s=%d,b=%d => r=%d,g=%d, b=%d, w=%d,\n",(int)led_hue,(int)led_saturation,(int)led_brightness, target_color.red,target_color.green, target_color.blue, target_color.white );
+    } else {
+        printf("led srtip off\n");
+        target_color.red = 0;
+        target_color.green = 0;
+        target_color.blue = 0;
+        target_color.white = 0;
+    }
+    
+ //   current_color.red += ((target_color.red * 256) - current_color.red) >> LPF_SHIFT ;
+ //   current_color.green += ((target_color.green * 256) - current_color.green) >> LPF_SHIFT ;
+ //   current_color.blue += ((target_color.blue * 256) - current_color.blue) >> LPF_SHIFT ;
+
+    
+    
+    led_strip_send(&target_color);
 }
 
 void led_identify_task(void *_args) {
+
     printf("LED identify\n");
-    
-    rgb_color_t color = target_color;
+
     rgb_color_t black_color = { { 0, 0, 0, 0 } };
     rgb_color_t white_color = { { 128, 128, 128, 128 } };
     
     for (int i=0; i<3; i++) {
         for (int j=0; j<2; j++) {
-            target_color = white_color;
+            led_strip_send (&black_color);
             vTaskDelay(100 / portTICK_PERIOD_MS);
-            
-            target_color = black_color;
+            led_strip_send (&white_color);
             vTaskDelay(100 / portTICK_PERIOD_MS);
+
         }
         
         vTaskDelay(250 / portTICK_PERIOD_MS);
     }
     
-    target_color = color;
-    
+
+    led_strip_send (&target_color);
     vTaskDelete(NULL);
 }
 
@@ -156,6 +256,7 @@ void led_on_set(homekit_value_t value) {
     }
     
     led_on = value.bool_value;
+    led_strip_set();
 }
 
 homekit_value_t led_brightness_get() {
@@ -168,6 +269,7 @@ void led_brightness_set(homekit_value_t value) {
         return;
     }
     led_brightness = value.int_value;
+    led_strip_set();
 }
 
 homekit_value_t led_hue_get() {
@@ -180,6 +282,7 @@ void led_hue_set(homekit_value_t value) {
         return;
     }
     led_hue = value.float_value;
+    led_strip_set();
 }
 
 homekit_value_t led_saturation_get() {
@@ -192,6 +295,7 @@ void led_saturation_set(homekit_value_t value) {
         return;
     }
     led_saturation = value.float_value;
+    led_strip_set();
 }
 
 homekit_accessory_t *accessories[] = {
@@ -227,6 +331,7 @@ homekit_accessory_t *accessories[] = {
                                    .getter = led_saturation_get,
                                    .setter = led_saturation_set
                                    ),
+            &ota_trigger,
             NULL
         }),
         NULL
@@ -239,44 +344,6 @@ homekit_server_config_t config = {
     .password = "111-11-111"    
 };
 
-IRAM void multipwm_task(void *pvParameters) {
-    const TickType_t xPeriod = pdMS_TO_TICKS(LPF_INTERVAL);
-    TickType_t xLastWakeTime = xTaskGetTickCount();
-    
-    uint8_t pins[] = {RED_PWM_PIN, GREEN_PWM_PIN, BLUE_PWM_PIN};
-    
-    pwm_info_t pwm_info;
-    pwm_info.channels = 3;
-    
-    multipwm_init(&pwm_info);
-    multipwm_set_freq(&pwm_info, 65535);
-    for (uint8_t i=0; i<pwm_info.channels; i++) {
-        multipwm_set_pin(&pwm_info, i, pins[i]);
-    }
-    
-    while(1) {
-        if (led_on) {
-            // convert HSI to RGBW
-            hsi2rgb(led_hue, led_saturation, led_brightness, &target_color);
-        } else {
-            target_color.red = 0;
-            target_color.green = 0;
-            target_color.blue = 0;
-        }
-        
-        current_color.red += ((target_color.red * 256) - current_color.red) >> LPF_SHIFT ;
-        current_color.green += ((target_color.green * 256) - current_color.green) >> LPF_SHIFT ;
-        current_color.blue += ((target_color.blue * 256) - current_color.blue) >> LPF_SHIFT ;
-        
-        multipwm_stop(&pwm_info);
-        multipwm_set_duty(&pwm_info, 0, current_color.red);
-        multipwm_set_duty(&pwm_info, 1, current_color.green);
-        multipwm_set_duty(&pwm_info, 2, current_color.blue);
-        multipwm_start(&pwm_info);
-        
-        vTaskDelayUntil(&xLastWakeTime, xPeriod);
-    }
-}
 
 void create_accessory_name() {
     
@@ -309,18 +376,16 @@ void create_accessory_name() {
 void user_init(void) {
     //uart_set_baud(0, 115200);
     
-    // This example shows how to use same firmware for multiple similar accessories
-    // without name conflicts. It uses the last 3 bytes of accessory's MAC address as
-    // accessory name suffix.
 
     create_accessory_name();
     
+    pwm_info.channels = 4;
+    led_strip_init();
+
     int c_hash=ota_read_sysparam(&manufacturer.value.string_value,&serial.value.string_value,
                                  &model.value.string_value,&revision.value.string_value);
     if (c_hash==0) c_hash=1;
     config.accessories[0]->config_number=c_hash;
-    
-    xTaskCreate(multipwm_task, "multipwm", 256, NULL, 2, NULL);
     
     homekit_server_init(&config);
 }
